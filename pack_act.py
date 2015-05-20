cf24043bdc73a0d446d5e7c97842620011c2b23a
Gonzalo Odiard <godiard@gmail.com>
2015-05-20 17:54:14 -0300

    Add changelog and gbp.conf files

--- pack_act.py
+++ pack_act.py
@@ -10,10 +10,11 @@ from ConfigParser import ConfigParser, ParsingError
 
 # in this case will be
 activity_sources_path = './sugarlabs-calculate'
-distribution = 'debian'
+distro = 'debian'
 package_name = 'sugar-calculate-activity'
 
 ACT_SECTION = 'Activity'
+PKG_SECTION = 'Package'
 
 # read activity.info file
 activity_info_path = os.path.join(activity_sources_path,
@@ -28,23 +29,56 @@ activity_info.readfp(open(activity_info_path))
 activity_version = activity_info.get(ACT_SECTION, 'activity_version')
 print "Activity version", activity_version
 
+# read $distro.info file
+distro_info_path = os.path.join(activity_sources_path,
+                                  'activity/%s.info' % distro)
+if not os.path.exists(distro_info_path):
+    print "FATAL ERROR: no %s.info file available on path '%s'" % \
+        (distro, distro_info_path)
+    exit()
+
+distro_info = ConfigParser()
+distro_info.readfp(open(distro_info_path))
+pkg_version = distro_info.get(PKG_SECTION, 'version')
+print "Package version", pkg_version
+
+
+
 # create the directory to work
-data_path = os.path.join(package_name, distribution)
+data_path = os.path.join(package_name, distro)
 if not os.path.exists(data_path):
     os.makedirs(data_path)
 
-if distribution == 'debian':
+if distro == 'debian':
     # changelog
+    # TODO: check previous changes
+    with open(os.path.join(data_path, 'changelog'), 'w') as changelog_file:
+        changelog_file.write('%s (%s-%s) UNRELEASED; urgency=low\n\n' %
+                             (package_name, activity_version, pkg_version))
+        changelog_file.write('  * Initial version for Debian.\n')
 
     # compat
     with open(os.path.join(data_path, 'compat'), 'w') as compat_file:
         compat_file.write('8\n')
     # control
     # control.in
+
     # gbp.conf
+    gbp_content = """# Configuration file for git-buildpackage and friends
+
+[DEFAULT]
+pristine-tar = True
+sign-tags = True
+
+[git-buildpackage]
+compression = bzip2
+"""
+    with open(os.path.join(data_path, 'gbp.conf'), 'w') as gbp_conf_file:
+        gbp_conf_file.write(gbp_content)
+
     # README.source
     # rules
     # source
     # watch
 else:
-    print "Distribution '%s' unknown" % distribution
+    print "Distribution '%s' unknown" % distro
